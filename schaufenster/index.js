985a16c843b90dcdef99f91088e13d033347910c
atelierdufour <wischt.zinn27@icloud.com>
2026-03-17 13:20:32 +0100

    big update

--- schaufenster/index.js
+++ schaufenster/index.js
@@ -2,15 +2,6 @@ fetch(`/schaufenster/api/properties.json`)
   .then(response => response.json())
   .then(Propertiesjson => {
 
-      fetch(`/schaufenster/api/object/${Propertiesjson.last_post}/properties.json`)
-    .then(response => response.json())
-    .then(lastObjectPropertiesjson => {
-
-    document.getElementById('main-title').textContent = lastObjectPropertiesjson.title;
-    document.getElementById('main-description').textContent = lastObjectPropertiesjson.description;
-    document.getElementById("last-img").src = `/schaufenster/api/object/${Propertiesjson.last_post}/img.jpg`;
-  })
-
     for (let i = Propertiesjson.last_post; i >= 1; i--) {
       let number = i.toString().padStart(3, '0');
 
